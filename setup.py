87f7ba7ac4c6e08a8011156307e552ccfce4dbdb
imWildCat <wildcat.name@gmail.com>
2018-04-08 18:24:52 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='sogou-translate',
-    packages=['sogou-translate'],
     python_requires='>=3.6.0',
-    py_modules=['sogou_translate1'],
+    # If your package is a single module, use this instead of 'packages':
+    py_modules=['sogou_translate'],
     version='0.1',
     description='The Python client for Sogou Translate Service',
     author='Daohan Chong',
@@ -12,7 +12,14 @@ setup(
     url='https://github.com/imWildCat/sogou-translate',
     download_url='https://github.com/imWildCat/sogou-translate/archive/0.1.tar.gz',
     keywords=['translate', 'api', 'sougou'],
-    classifiers=[],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
     install_requires=[
         'requests',
     ]
